60a1879c0c3b75abae9f4a77446c8f93cac8b5ea
GoldenJayz <45317852+GoldenJayz@users.noreply.github.com>
2020-11-01 07:55:18 -0500

    Update lisa.js

--- lisa.js
+++ lisa.js
@@ -1,5 +1,5 @@
 const Discord = require("discord.js");
-const { prefix, token } = require("./config.json");
+const { prefix } = require("./config.json");
 const ytdl = require("ytdl-core");
 const search = require("ytsr")
 
